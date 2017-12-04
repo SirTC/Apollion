fc31dae184ed454a799f6c5997c83afaf7091db6
Josh Sweitzer <JSweitzer@connectionseducation.com>
2017-12-04 15:27:47 -0500

    nav updates. added pve pvp pages

---
+++ Apollion/Controllers/StatsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Apollion.Controllers
+{
+    public class StatsController : Controller
+    {
+        // GET: Stats
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PVE()
+        {
+            return View();
+        }
+
+        public ActionResult PVP()
+        {
+            return View();
+        }
+    }
+}
